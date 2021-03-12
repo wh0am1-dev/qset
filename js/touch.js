4572b2a576cf4a6bb680e8408a1fbc9a31731fe9
Carlos Aguilar <hi@wh0am1.dev>
2021-03-12 16:58:06 +0100

    fix: rnd quote on tap hold

--- js/touch.js
+++ js/touch.js
@@ -2,7 +2,7 @@ $(() => {
   if (isMobile()) {
     $.event.special.tap.emitTapOnTaphold = false
     $('#qtitlem').on('tap', nextColor)
-    $('#qtitlem').on('taphold', nextColor)
+    $('#qtitlem').on('taphold', rndQuote)
     $('#qtitlem').on('swipeleft', nextQuote)
     $('#qtitlem').on('swiperight', prevQuote)
     $('#qtitlem').on('contextmenu', e => e.preventDefault())
